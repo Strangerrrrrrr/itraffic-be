a3963886fbdc9ed437a39b1ab5fafdc7c0079f40
YunYouJun <me@yunyoujun.cn>
2018-05-05 15:21:39 +0800

    laravel-admin add user

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -16,24 +16,24 @@ class HomeController extends Controller
         return Admin::content(function (Content $content) {
 
             $content->header('Dashboard');
-            $content->description('Description...');
+            $content->description('依赖环境');
 
-            $content->row(Dashboard::title());
+            // $content->row(Dashboard::title());
 
-            $content->row(function (Row $row) {
+            // $content->row(function (Row $row) {
 
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::environment());
-                });
+            //     $row->column(4, function (Column $column) {
+            //         $column->append(Dashboard::environment());
+            //     });
 
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::extensions());
-                });
+            //     $row->column(4, function (Column $column) {
+            //         $column->append(Dashboard::extensions());
+            //     });
 
-                $row->column(4, function (Column $column) {
-                    $column->append(Dashboard::dependencies());
-                });
-            });
+            //     $row->column(4, function (Column $column) {
+            //         $column->append(Dashboard::dependencies());
+            //     });
+            // });
         });
     }
 }
---
+++ app/Admin/Controllers/UserController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\User;
+
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Layout\Content;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\ModelForm;
+
+class UserController extends Controller
+{
+    use ModelForm;
+
+    /**
+     * Index interface.
+     *
+     * @return Content
+     */
+    public function index()
+    {
+        return Admin::content(function (Content $content) {
+
+            $content->header('用户管理');
+            $content->description('管理用户信息');
+
+            $content->body($this->grid());
+        });
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param $id
+     * @return Content
+     */
+    public function edit($id)
+    {
+        return Admin::content(function (Content $content) use ($id) {
+
+            $content->header('编辑用户信息');
+            $content->description('编辑用户信息');
+
+            $content->body($this->form()->edit($id));
+        });
+    }
+
+    /**
+     * Create interface.
+     *
+     * @return Content
+     */
+    public function create()
+    {
+        return Admin::content(function (Content $content) {
+
+            $content->header('新增用户');
+            $content->description('内部添加新用户');
+
+            $content->body($this->form());
+        });
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        return Admin::grid(User::class, function (Grid $grid) {
+            $grid->paginate(20);
+
+            $grid->id('ID')->sortable();
+            $grid->column('name', '用户名');
+            $grid->column('real_name', '真实姓名');
+            $grid->column('sex', '性别');
+            $grid->column('identity', '身份证');
+            $grid->column('phone', '联系方式');
+
+            $grid->column('pass', '审核通过')->display(function ($pass) {
+                if ($pass == 0) {
+                    return '否';
+                } else {
+                    return '是';
+                }
+            });
+
+            $grid->created_at('注册时间');
+            $grid->updated_at('更新时间');
+
+            // filter($callback)方法用来设置表格的简单搜索框
+            $grid->filter(function ($filter) {
+                $filter->like('name', '用户名');
+                // 设置created_at字段的范围查询
+                // $filter->between('created_at', 'Created Time')->datetime();
+            });
+        });
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        return Admin::form(User::class, function (Form $form) {
+
+            $form->display('id', 'ID');
+
+            $form->text('name', '姓名');
+            $form->text('real_name', '真实姓名');
+            $form->radio('sex', '性别')->options(['男' => '男', '女'=> '女'])->default('男');
+            $form->text('identity', '身份证');
+            $form->text('phone', '联系方式');
+
+            $form->radio('pass', '审核通过')->options([1 => '是', 0 => '否'])->default('男');
+
+            $form->display('created_at', '创建时间');
+            $form->display('updated_at', '更新时间');
+        });
+    }
+}
